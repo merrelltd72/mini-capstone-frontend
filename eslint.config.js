c5129709ab484a1a5891aaf13e8fe3e52c38f111
Tyron Merrell <tyron.merrell@gmail.com>
2025-11-06 20:30:53 -0600

    Updated npm packages and eslint config.

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: [
+      "**/*.js",
+      "**/*.cjs",
+      "**/*.mjs",
+      "**/*.jsx",
+      "**/*.ts",
+      "**/*.tsx",
+    ],
+    rules: {
+      "prefer-const": "warn",
+      "no-constant-binary-expression": "error",
+    },
+  },
+]);
